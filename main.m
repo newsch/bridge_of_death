763d3d584624d92c0c1baf20773825d54f8f37d4
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2018-04-10 21:53:03 -0400

    Stop and think about what we've done
    - plot outputted velocities
    - change speeds with a constant

--- main.m
+++ main.m
@@ -3,20 +3,24 @@
 %% Setup
 max_speed = 0.3;  % max speed of the robot
 d = 0.24;  % distance between wheels
-c = 0.125;  % speed up/slow down
-t_step = 0.1125;
-u = [0:t_step:2*pi]';  % values to sweep through
+c = 0.25;  % speed up/slow down
+u_step = 0.25;
+u = [0:u_step:pi/c]';  % values to sweep through
 
-% recommended ellipse
-r = [0.5*cos(u), 0.75*sin(u)];
+% % circle with radius R
+% R = 0.9144;  % 3 feet in meters
+% r = [R*cos(u*c),R*sin(u*c)];
 
-% % Challenge 1
-% r = [0.3960*cos(2.65*(u + 1.4)), 0.99*sin(u + 1.4)];
-% 
+% % recommended ellipse
+% r = [0.5*cos(u*c), 0.75*sin(u*c)];
+
+% Challenge 1
+r = [0.3960*cos(2.65*(c*u + 1.4)), 0.99*sin(c*u + 1.4)];
+
 % % Challenge 2
 % a = 0.4;
 % l = 0.4;
-% r = [-2*a*((l-cos(u)*cos(u)+(1-l)), 2*a*(l-cos(u))*sin(u)];
+% r = [-2*a*((l-cos(u)).*cos(u)+(1-l)), 2*a*(l-cos(u)).*sin(u)];
 
 %% Calculate wheel velocities
 T = diff(r) ./ diff(u);  % velocity vector
@@ -27,24 +31,48 @@ N3 = [N, zeros(size(N(:, 1)))];  % add a third dim to N
 Omega = cross(T_hat3(1:end - 1, :), N3);  % rotational velocities
 V = sqrt(sum(T.^2, 2));  % linear velocities
 
-
 Vr = V(1:end-1,:) + d * sum(Omega, 2);
 Vl = V(1:end-1,:) - d * sum(Omega, 2);
-V_comb = [Vr, Vl];
-Vr = Vr * (0.3 / max(max(V_comb)));
-Vl = Vl *(0.3 / max(max(V_comb)));
-display(max(Vr))
-display(max(Vl))
+
+% V_comb = [Vr, Vl];
+% max_v = max(max(V_comb));
+% Omega = Omega * (0.3 / max_v);
+% V = V * (0.3 / max_v);
+% 
+% Vr = V(1:end-1,:) + d * sum(Omega, 2);
+% Vl = V(1:end-1,:) - d * sum(Omega, 2);
 
 %% Calculate each time step:
 distances = sqrt(sum(diff(r).^2, 2));
 times = distances ./ V;
 
-%% Run the robot
-disp(max(Vr))
-disp(max(Vl))
-if max(Vr) > max_speed || max(Vl) > max_speed
-    disp("WARNING: Velocities greater than max of "+string(max_speed)+".")
+%% Plot the predicted course
+figure; hold on
+plot(r(:,1),r(:,2))
+pos = [0, 0];  % position of robot
+head = 0;  % heading of robot
+plot(pos(1),pos(2),'b*')
+for i = 1:length(u) - 2
+    dt = times(i);
+    That = [cos(head), sin(head)];
+    lin_vel = (Vl(i) + Vr(i))/2;
+    ang_vel = (Vr(i) - Vl(i))/d;
+    drdpos = lin_vel*That;
+    drdhead = ang_vel;
+    plot(pos(1),pos(2),'r*')
+%     quiver(pos(1),pos(2), That(1),That(2))
+    new_pos = pos + drdpos*dt;
+    new_head = head + drdhead*dt;
+    pos = new_pos;
+    head = new_head;
 end
+plot(pos(1),pos(2),'g*')
 
-runCourse(ones(size(Vr))*t_step,Vr,Vl)
+% %% Run the robot
+% disp(max(Vr))
+% disp(max(Vl))
+% if max(Vr) > max_speed || max(Vl) > max_speed
+%     disp("WARNING: Velocities greater than max of "+string(max_speed)+".")
+% end
+% 
+% runCourse(times(1:end-1,:),Vr,Vl)
